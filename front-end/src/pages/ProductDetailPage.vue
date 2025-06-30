18d80301c5837b0c10764ced3522bdcab11c90a4
Lucas Pereira <lucasferrari4@gmail.com>
2025-06-30 11:43:35 -0600

    01_08_b->e

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Product Detail Page</h1>
+  <div class="img-wrap">
+    <img :src="product.imageName" alt="Product Image" />
+  </div>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <h3 class="price">{{ product.price }}</h3>
+    <button class="add-to-cart">Add t cart</button>
+  </div>
 </template>
 
 <script>
+import { products } from "../temp-data.js";
 export default {
   name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find(
+        (product) => 
+           product.id === this.$route.params.productId
+      ),
+    };
+  }
 }
 </script>
